fa20f0bdde9c31c7d6d15ba41995b7959c40a484
Chris Southam <me@csoutham.com>
2021-05-03 13:15:48 +0100

    Update BunnyCDNAdapter.php

--- src/Filesystem/BunnyCDNAdapter.php
+++ src/Filesystem/BunnyCDNAdapter.php
@@ -8,48 +8,49 @@
 namespace Katzen48\LaravelBunnyCdn\Filesystem;
 
 use BunnyCDN\Storage\Exceptions\BunnyCDNStorageException;
-use Illuminate\Support\Facades\Log;
-use Katzen48\LaravelBunnyCdn\Filesystem\BunnyCDNStorage;
 use League\Flysystem\Config;
+use PlatformCommunity\Flysystem\BunnyCDN\BunnyCDNAdapter as Adapter;
 use PlatformCommunity\Flysystem\BunnyCDN\Util;
 
-class BunnyCDNAdapter extends \PlatformCommunity\Flysystem\BunnyCDN\BunnyCDNAdapter
+class BunnyCDNAdapter extends Adapter
 {
-    public function __construct(BunnyCDNStorage $bunnyCDNStorage)
-    {
-        parent::__construct($bunnyCDNStorage);
-    }
+	public function __construct(BunnyCDNStorage $bunnyCDNStorage)
+	{
+		parent::__construct($bunnyCDNStorage);
+	}
 
-    public function writeStream($path, $resource, Config $config)
-    {
-        try {
-            $fileStream = $resource;
-            if ($fileStream === false) {
-                throw new BunnyCDNStorageException('The resource could not be opened.');
-            }
-            $dataLength = fstat($resource)['size'];
-            $storage = $this->getStorage();
-            $normalizedPath = $this->fullPath($path);
+	public function writeStream($path, $resource, Config $config)
+	{
+		try {
+			$fileStream = $resource;
+			
+			if ($fileStream === false) {
+				throw new BunnyCDNStorageException('The resource could not be opened.');
+			}
+			$dataLength = fstat($resource)['size'];
+			$storage = $this->getStorage();
+			$normalizedPath = $this->fullPath($path);
 
-            return $storage->sendHttpRequest($normalizedPath, 'PUT', $fileStream, $dataLength);
-        } catch (BunnyCDNStorageException $e) {
-            report($e);
+			return $storage->sendHttpRequest($normalizedPath, 'PUT', $fileStream, $dataLength);
+		} catch (BunnyCDNStorageException $e) {
+			report($e);
 
-            return false;
-        }
-    }
+			return false;
+		}
+	}
 
-    /**
-     * @param $path
-     * @return string
-     */
-    private function fullPath($path): string
-    {
-        return '/' . $this->applyPathPrefix('/' . Util::normalizePath($path));
-    }
+	/**
+	 * @param $path
+	 *
+	 * @return string
+	 */
+	private function fullPath($path): string
+	{
+		return '/' . $this->applyPathPrefix('/' . Util::normalizePath($path));
+	}
 
-    private function getStorage() : BunnyCDNStorage
-    {
-        return $this->bunnyCDNStorage;
-    }
+	private function getStorage(): BunnyCDNStorage
+	{
+		return $this->bunnyCDNStorage;
+	}
 }
